fa8dc1cd1c3e6907aa530508840c4e30e2fe1128
Ziang Lu <ziangl@andrew.cmu.edu>
2018-03-01 17:53:22 -0500

    finished longest common substring problem

---
+++ 3-Greedy Algorithms, Minimum Spanning Tree, and Dynamic Programming/2-Dynamic Programming/3-Sequence Alignment Problem/Two Strings Problems Using Dynamic Progamming/1-Longest Common Substring Problem/LongestCommonSubstringFinder.java
@@ -0,0 +1,78 @@
+/**
+ * Given two strings X and Y, find the longest common substring.
+ *
+ * Algorithm: (Dynamic programming)
+ * Find the longest common suffix for all pairs of prefixes of the strings.
+ * => The maximum of these longest common suffixes of possible prefixes must be
+ *    the longest common substring.
+ *
+ * Let LCS(X_i, Y_j) be the longest common suffix for the subproblem with the
+ * prefix of i characters of X and j characters of Y, respectively, then
+ * consider the final characters x_i and y_j:
+ * 1. x_i and y_j are different:
+ *    => LCS(X_i, Y_j) = 0
+ * 2. x_i and y_j are the same:
+ *    Let X_i' = {X_i - x_i} and Y_j' = {Y_j - y_j}
+ *    => LCS(X_i, Y_j) = LCS(X_i', Y_j') + x_i
+ */
+
+public class LongestCommonSubstringFinder {
+
+    /**
+     * Subproblem solutions.
+     * Since there are only O(mn) subproblems, the first time we solve a
+     * subproblem, we can cache its solution in a global take for O(1) lookup
+     * time later on.
+     */
+    private int[][] subproblems;
+
+    /**
+     * Finds the longest common substring of the given two strings in a
+     * bottom-up way.
+     * @param x first string
+     * @param y second string
+     * @return longest common substring
+     */
+    public String findLongestCommonSubstring(String x, String y) {
+        // Check whether the input strings are null or empty
+        if ((x == null) || (x.length() == 0) || (y == null) || (y.length() == 0)) {
+            throw new IllegalArgumentException("The input strings should not be null or empty.");
+        }
+
+        int m = x.length(), n = y.length();
+        // Initialization
+        subproblems = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; ++i) {
+            subproblems[i][0] = 0;
+        }
+        for (int j = 0; j <= n; ++j) {
+            subproblems[0][j] = 0;
+        }
+        // Bottom-up calculation
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                char xFinal = x.charAt(i - 1), yFinal = y.charAt(j - 1);
+                if (xFinal != yFinal) {
+                    subproblems[i][j] = 0;
+                } else {
+                    subproblems[i][j] = subproblems[i - 1][j - 1] + 1;
+                }
+            }
+        }
+
+        // Find the maximum of the longest common suffixes of possible prefixes, which is exactly the longest common
+        // substring
+        int iMax = 0, maxLength = subproblems[0][0];
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                if (subproblems[i][j] > maxLength) {
+                    iMax = i;
+                    maxLength = subproblems[i][j];
+                }
+            }
+        }
+        return x.substring(iMax - maxLength, iMax);
+        // Overall running time complexity is O(mn).
+    }
+
+}
---
+++ 3-Greedy Algorithms, Minimum Spanning Tree, and Dynamic Programming/2-Dynamic Programming/3-Sequence Alignment Problem/Two Strings Problems Using Dynamic Progamming/1-Longest Common Substring Problem/longest_common_substring.py
@@ -0,0 +1,60 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Given two strings X and Y, find the longest common substring.
+
+Algorithm: (Dynamic programming)
+Find the longest common suffix for all pairs of prefixes of the strings.
+=> The maximum of these longest common suffixes of possible prefixes must be the
+   longest common substring.
+
+Let LCS(X_i, Y_j) be the longest common suffix for the subproblem with the
+prefix of i characters of X and j characters of Y, respectively, then consider
+the final characters x_i and y_j:
+1. x_i and y_j are different:
+   => LCS(X_i, Y_j) = 0
+2. x_i and y_j are the same:
+   Let X_i' = {X_i - x_i} and Y_j' = {Y_j - y_j}
+   => LCS(X_i, Y_j) = LCS(X_i', Y_j') + x_i
+"""
+
+__author__ = 'Ziang Lu'
+
+
+class IllegalArgumentError(ValueError):
+    pass
+
+
+def find_longest_common_substring(x, y):
+    # Check whether the input strings are None or empty
+    if x is None or len(x) == 0 or y is None or len(y) == 0:
+        raise IllegalArgumentError('The input strings should noe bt None or '
+                                   'empty.')
+
+    m, n = len(x), len(y)
+    # Initialization
+    subproblems = [[0 for j in range(n + 1)] for i in range(m + 1)]
+    for i in range(m + 1):
+        subproblems[i][0] = 0
+    for j in range(n + 1):
+        subproblems[0][j] = 0
+    # Bottom-up calculation
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            x_final, y_final = x[i - 1], y[j - 1]
+            if x_final != y_final:
+                subproblems[i][j] = 0
+            else:
+                subproblems[i][j] = subproblems[i - 1][j - 1] + 1
+
+    # Find the maximum of the longest common suffix of possible prefixes, which
+    # is exactly the longest common substring
+    i_max, max_length = 0, subproblems[0][0]
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if subproblems[i][j] > max_length:
+                i_max = i
+                max_length = subproblems[i][j]
+    return x[i_max - max_length:i_max]
+    # Overall running time complexity: O(mn)
