3106a426406446cb0c58eeac5edf1d2da7210b52
Ziang Lu <ziangl@andrew.cmu.edu>
2018-03-01 15:32:14 -0500

    finished optimal bst with Knuth's optimization

---
+++ 3-Greedy Algorithms, Minimum Spanning Tree, and Dynamic Programming/2-Dynamic Programming/4-Optimal BST Problem/OptimalBSTFinderWithOptimization.java
@@ -0,0 +1,154 @@
+/**
+ * Same as the optimal BST problem.
+ *
+ * Algorithm: Similar to the ordinary optimal BST problem, but with Knuth's
+ * optimization.
+ * The key point is the monotony of the roots:
+ * root(i, j-1) <= root(i, j) <= root(i+1, j)
+ * [Proof ?????]
+ * Thus, by keeping track of the root, we can significantly reduce the number
+ * of brute-force searches when searching for the root of a subproblem.
+ */
+
+public class OptimalBSTFinderWithOptimization {
+
+    /**
+     * Default value for subproblem solutions.
+     */
+    private static final int DEFAULT_SUBPROBLEM_SOL = 0;
+
+    /**
+     * Roots of the subproblems.
+     */
+    private int[][] roots;
+    /**
+     * Subproblem solutions.
+     * Since there are only O(n^2) subproblems, the first time we solve a
+     * subproblem, we can cache its solution in a global take for O(1) lookup
+     * time later on.
+     */
+    private int[][] subproblems;
+
+    /**
+     * Finds the optimal BST for items with the given weight distribution, and
+     * calculates its weighted search time (WST), in a straightforward way.
+     * @param weights weight distribution of the items
+     * @return optimal WST
+     */
+    public int findOptimalBSTStraightforward(int[] weights) {
+        // Check whether the input array is null or empty
+        if ((weights == null) || (weights.length == 0)) {
+            throw new IllegalArgumentException("The input weight distribution should not be null or empty.");
+        }
+
+        int n = weights.length;
+        initializeSubproblemSols(n);
+        return findOptimalBSTHelper(weights, 0, n - 1);
+        // With memoization, the overall running time complexity is O(n^3).
+    }
+
+    /**
+     * Private helper method to initialize the subproblem solutions.
+     * @param n number of items
+     */
+    private void initializeSubproblemSols(int n) {
+        subproblems = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                subproblems[i][j] = DEFAULT_SUBPROBLEM_SOL;
+            }
+        }
+        // Running time complexity: O(n^2)
+    }
+
+    /**
+     * Private helper method to find the optimal BST for the given contiguous
+     * items with the given weight distribution, and calculates its WST,
+     * recursively.
+     * @param weights weights of the items
+     * @param start starting item of the contiguous items
+     * @param end ending item of the contiguous items
+     * @return optimal WST of the given continuous items
+     */
+    private int findOptimalBSTHelper(int[] weights, int start, int end) {
+        // Base case 1: start > end
+        if (start > end) {
+            return 0;
+        }
+        // Recursive case
+        if (subproblems[start][end] != DEFAULT_SUBPROBLEM_SOL) {
+            return subproblems[start][end];
+        }
+        int minWST = Integer.MAX_VALUE;
+        int weightSum = 0;
+        for (int k = start; k <= end; ++k) {
+            weightSum += weights[k];
+        }
+        // Use Knuth's optimization:
+        // root(i, j-1) <= root(i, j) <= root(i+1, j)
+        for (int r = start; r <= end; ++r) {
+            int currWST = findOptimalBSTHelper(weights, start, r - 1) + findOptimalBSTHelper(weights, r + 1, end)
+                    + weightSum;
+            if (currWST < minWST) {
+                minWST  = currWST;
+            }
+        }
+        subproblems[start][end] = minWST;
+        return minWST;
+    }
+
+    /**
+     * Finds the optimal BST for items with the given weight distribution, and
+     * calculates its weighted search time (WST), in an improved bottom-up way.
+     * @param weights weight distribution of the items
+     * @return optimal WST
+     */
+    public int findOptimalBST(int[] weights) {
+        // Check whether the input array is null or empty
+        if ((weights == null) || (weights.length == 0)) {
+            throw new IllegalArgumentException("The input weight distribution should not be null or empty.");
+        }
+
+        int n = weights.length;
+        // Initialization
+        roots = new int[n][n];
+        subproblems = new int[n][n];
+        int nItem = 1;
+        for (int i = 0; i < n; ++i) {
+            roots[i][i] = i;
+            subproblems[i][i] = weights[i];
+        }
+        // Bottom-up calculation
+        for (nItem = 2; nItem <= n; ++nItem) {
+            for (int start = 0; start < (n - nItem + 1); ++start) {
+                int end = Math.min(start + nItem - 1, n - 1);
+                int rootWithMinWST = -1, minWST = Integer.MAX_VALUE;
+                int weightSum = 0;
+                for (int k = start; k <= end; ++k) {
+                    weightSum += weights[k];
+                }
+                // Use Knuth optimization:
+                // root(i, j-1) <= root(i, j) <= root(i+1, j)
+                for (int r = roots[start][end - 1]; r <= roots[start + 1][end]; ++r) {
+                    int leftSubtreeWST = 0, rightSubtreeWST = 0;
+                    if (start <= (r - 1)) {
+                        leftSubtreeWST = subproblems[start][r - 1];
+                    }
+                    if ((r + 1) <= end) {
+                        rightSubtreeWST = subproblems[r + 1][end];
+                    }
+                    int currWST = leftSubtreeWST + rightSubtreeWST + weightSum;
+                    if (currWST < minWST) {
+                        rootWithMinWST = r;
+                        minWST = currWST;
+                    }
+                }
+                roots[start][end] = rootWithMinWST;
+                subproblems[start][end] = minWST;
+            }
+        }
+        return subproblems[0][n - 1];
+        // Overall running time complexity: O(n^2) [Analysis ?????]
+    }
+
+}
--- 3-Greedy Algorithms, Minimum Spanning Tree, and Dynamic Programming/2-Dynamic Programming/4-Optimal BST Problem/optimal_bst_with_optimization.py
+++ 3-Greedy Algorithms, Minimum Spanning Tree, and Dynamic Programming/2-Dynamic Programming/4-Optimal BST Problem/optimal_bst_with_optimization.py
@@ -2,32 +2,15 @@
 # -*- coding: utf-8
 
 """
-Given items 1, 2, ..., n (assume in sorted order), with positive frequencies
-p_1, p_2, ..., p_n, compute a valid BST that minimizes the weighted search time
-(WST):
-C(T) = sum(p_i * [search time for item-i])
-where the search time for item-i is defined to be the number of nodes you have
-to visit until you find it (inclusive).
+Same as the optimal BST problem.
 
-Optimal substructure lemma:
-Suppose an optimal BST for items 1, 2, ..., n T HAS ROOT r, left subtree T_L and
-right subtree T_R, then T_L is optimal for items 1, 2, ..., r-1 and T_R is
-optimal for items r+1, r+2, ..., n
--Proof:
-Suppose T_L is not optimal for items 1, 2, ..., r-1 with C(T_L*) < C(T_L).
-(The other case is similar.)
-Thus, we can obtain a T* from T by replacing the left subtree by T*.
-=>
-C(T) = sum(p_i * [search time for item-i in T]
-     = C(T_L) + sum(p_i * 1) for 1<=i<=r-1 + p_r * 1 + C(T_R) + sum(p_i * 1) for
-       r+1<=i<=n)
-     = C(T_L) + C(T_R) + sum(p_i) for 1<=i<=n
-     < C(T_L*) + C(T_R) + const = C(T*)
-Thus by proof by contradiction, T_L must be optimal for items 1, 2, ..., r-1.
-
-Algorithm: (Dynamic programming)
-Suppose 1<=i<=j<=n,
-C(T_{i,j}) = min_r (i<=r<=j) (C(T_L) + C(T_R) + sum(p_k) for i<=k<=j)
+Algorithm: Similar to the ordinary optimal BST problem, but with Knuth's
+optimization.
+The key point is the monotony of the roots:
+root(i, j-1) <= root(i, j) <= root(i+1, j)
+[Proof ?????]
+Thus, by keeping track of the root, we can significantly reduce the number of
+brute-force searches when searching for the root of a subproblem.
 """
 
 __author__ = 'Ziang Lu'
@@ -105,19 +88,23 @@ def find_optimal_bst(weights):
 
     n = len(weights)
     # Initialization
+    roots = [[0 for j in range(n)] for i in range(n)]
     subproblems = [[0 for j in range(n)] for i in range(n)]
     n_item = 1
     for i in range(n):
+        roots[i][i] = i
         subproblems[i][i] = weights[i]
     # Bottom-up calculation
     for n_item in range(2, n + 1):
         for start in range(0, n - n_item + 1):
             end = min(start + n_item - 1, n - 1)
-            min_wst = sys.maxsize
+            root_with_min_wst, min_wst = -1, sys.maxsize
             weight_sum = 0
             for k in range(start, end + 1):
                 weight_sum += weights[k]
-            for r in range(start, end + 1):
+            # Use Knuth's optimization:
+            # root(i, j-1) <= root(i, j) <= root(i+1, j)
+            for r in range(roots[start][end - 1], roots[start + 1][end] + 1):
                 if start > r - 1:
                     left_subtree_wst = 0
                 else:
@@ -128,7 +115,9 @@ def find_optimal_bst(weights):
                     right_subtree_wst = subproblems[r + 1][end]
                 curr_wst = left_subtree_wst + right_subtree_wst + weight_sum
                 if curr_wst < min_wst:
+                    root_with_min_wst = r
                     min_wst = curr_wst
+            roots[start][end] = root_with_min_wst
             subproblems[start][end] = min_wst
     return subproblems[start][end]
-    # Overall running time complexity: O(n^3)
+    # Overall running time complexity: O(n^2) [Analysis ?????]
