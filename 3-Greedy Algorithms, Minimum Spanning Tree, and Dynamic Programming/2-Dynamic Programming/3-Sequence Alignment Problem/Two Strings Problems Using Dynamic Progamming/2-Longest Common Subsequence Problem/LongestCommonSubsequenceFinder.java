cb7bc7fc22c9fb4bbaa176e9ba3f430cfd3cc5f6
Ziang Lu <ziangl@andrew.cmu.edu>
2018-03-04 14:30:02 -0500

    finished longest common subsequence problem

---
+++ 3-Greedy Algorithms, Minimum Spanning Tree, and Dynamic Programming/2-Dynamic Programming/3-Sequence Alignment Problem/Two Strings Problems Using Dynamic Progamming/2-Longest Common Subsequence Problem/LongestCommonSubsequenceFinder.java
@@ -0,0 +1,97 @@
+/**
+ * Given two strings X and Y of length m and n, respectively, find the longest
+ * common subsequence (LCS).
+ *
+ * Algorithm: (Dynamic programming)
+ * Denote LCS(X, Y) to be the LCS of X and Y.
+ * Consider the final characters x_m and y_n:
+ * 1. x_m and y_n are the same:
+ *    Let X' = {X - x_m} and Y' = {Y - y_n}
+ *    => {LCS(X, Y) - x_m} is the LCS of X' and Y'.
+ *    => LCS(X, Y) = LCS(X', Y') + x_m
+ * 2. x_m and y_n are different:
+ *    => {LCS(X, Y) - x_m} is the LCS of X' and Y, and {LCS(X, Y) - y_n} is the
+ *       LCS of X and Y'.
+ *    => LCS(X, Y) = the longer one between LCS(X', Y) and LCS(X, Y')
+ *
+ * i.e.,
+ * Let LCS(X_i, Y_j) be the LCS for subproblems with the prefix of i characters
+ * of X and j characters of Y, respectively, then
+ * If x_i == y_j:
+ *     LCS(X_i, Y_j) = LCS(X_i - x_i, Y_j - y_j) + 1
+ * Else:
+ *     LCS(X_i, Y_j) = max(LCS(X_i - x_i, Y_j), LCS(X_i, Y_j - y_j))
+ */
+
+import java.util.LinkedList;
+
+public class LongestCommonSubsequenceFinder {
+
+    /**
+     * Subproblem solutions.
+     * Since there are only O(mn) subproblems, the first time we solve a
+     * subproblem, we can cache its solution in a global take for O(1) lookup
+     * time later on.
+     */
+    private int[][] subproblems;
+
+    /**
+     * Finds the longest common subsequence of the given two strings in an
+     * improved bottom-up way.
+     * @param x first string
+     * @param y second string
+     * @return longest common subsequence
+     */
+    public LinkedList<Character> longestCommonSubsequence(String x, String y) {
+        // Check whether the input strings are null or empty
+        if ((x == null) || (x.length() == 0) || (y == null) || (y.length() == 0)) {
+            throw new IllegalArgumentException("The input strings should not be null or empty.");
+        }
+
+        int m = x.length(), n = y.length();
+        // Initialization
+        subproblems = new int[m + 1][n + 1];
+        // Bottom-up calculation
+        for (int i = 1; i <= m; ++i) { 
+            for (int j = 1; j <= n; ++j) {
+                char xCurr = x.charAt(i - 1), yCurr = y.charAt(j - 1);
+                if (xCurr == yCurr) {
+                    subproblems[i][j] = subproblems[i - 1][j - 1] + 1;
+                } else {
+                    subproblems[i][j] = Math.max(subproblems[i - 1][j], subproblems[i][j - 1]);
+                }
+            }
+        }
+        return reconstructLongestCommonSubsequence(x, y);
+        // Overall running time complexity: O(mn)
+    }
+
+    /**
+     * Private helper method to reconstruct the longest common subsequence
+     * according to the optimal solution using backtracking.
+     * @param x first string
+     * @param y first string
+     * @return longest common subsequence
+     */
+    private LinkedList<Character> reconstructLongestCommonSubsequence(String x, String y) {
+        LinkedList<Character> lcs = new LinkedList<Character>();
+        int i = x.length(), j = y.length();
+        while ((i >= 1) && (j >= 1)) {
+            char xCurr = x.charAt(i - 1), yCurr = y.charAt(j - 1);
+            if (xCurr == yCurr) {
+                lcs.addFirst(xCurr);
+                --i;
+                --j;
+            } else {
+                if (subproblems[i][j] == subproblems[i - 1][j]) {
+                    --i;
+                } else {
+                    --j;
+                }
+            }
+        }
+        return lcs;
+        // Running time complexity: O(m + n)
+    }
+
+}
---
+++ 3-Greedy Algorithms, Minimum Spanning Tree, and Dynamic Programming/2-Dynamic Programming/3-Sequence Alignment Problem/Two Strings Problems Using Dynamic Progamming/2-Longest Common Subsequence Problem/longest_common_subsequence.py
@@ -0,0 +1,88 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Given two strings X and Y of length m and n, respectively, find the longest
+common subsequence (LCS).
+
+Algorithm: (Dynamic programming)
+Denote LCS(X, Y) to be the LCS of X and Y.
+Consider the final characters x_m and y_n:
+1. x_m and y_n are the same:
+   Let X' = {X - x_m} and Y' = {Y - y_n}
+   => {LCS(X, Y) - x_m} is the LCS of X' and Y'
+   => LCS(X, Y) = LCS(X', Y') + x_m
+2. x_m and y_n are different:
+   => {LCS(X, Y) - x_m} is the LCS of X' and Y, and {LCS(X, Y) - y_n} is the LCS
+      of X and Y'.
+   => LCS(X, Y) = the longer one between LCS(X', Y) and LCS(X, Y')
+
+i.e.,
+Let LCS(X_i, Y_j) be the LCS for subproblems with the prefix of i characters of
+X and j characters of Y, respectively, then
+If x_i == y_j:
+   LCS(X_i, Y_j) = LCS(X_i - x_i, Y_j - y_j) + 1
+Else:
+   LCS(X_i, Y_j) = max(LCS(X_i - x_i, Y_j), LCS(X_i, Y_j - y_j))
+"""
+
+__author__ = 'Ziang Lu'
+
+
+class IllegalArgumenError(ValueError):
+    pass
+
+
+def longest_common_subsequence(x, y):
+    """
+    Finds the longest common subsequence of the given strings in an improved
+    bottom-up way.
+    :param x: str
+    :param y: str
+    :return: list[char]
+    """
+    # Check whether the the input strings are None or empty
+    if x is None or len(x) == 0 or y is None or len(y) == 0:
+        raise IllegalArgumenError('The input strings should not be None or '
+                                  'empty.')
+
+    m, n = len(x), len(y)
+    # Initialization
+    subproblems = [[0 * (n + 1)] for i in range(m + 1)]
+    # Bottom-up calculation
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            x_curr, y_curr = x[i - 1], y[j - 1]
+            if x_curr == y_curr:
+                subproblems[i][j] = subproblems[i - 1][j - 1] + 1
+            else:
+                subproblems[i][j] = max(subproblems[i - 1][j],
+                                        subproblems[i][j - 1])
+    return _reconstruct_longest_common_subsequence(x, y,
+                                                   subproblems=subproblems)
+    # Overall running time complexity: O(mn)
+
+
+def _reconstruct_longest_common_subsequence(x, y, subproblems):
+    """
+    Private helper function to reconstruct the longest common subsequence
+    according to the optimal solution using backtracking.
+    :param x: str
+    :param y: str
+    :return: list[char]
+    """
+    lcs = []
+    i , j = len(x), len(y)
+    while i >= 1 and j >= 1:
+        x_curr, y_curr = x[i - 1], y[j - 1]
+        if x_curr == y_curr:
+            lcs.insert(0, x_curr)
+            i -= 1
+            j -= 1
+        else:
+            if subproblems[i][j] == subproblems[i - 1][j]:
+                i -= 1
+            else:
+                j -= 1
+    return lcs
+    # Running time complexity: O(m + n)
